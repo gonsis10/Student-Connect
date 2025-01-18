df9476f7670bd5e03e9c9a40e892f41f38690ac8
owenh1 <owenh1@students.cs.ubc.ca>
2025-01-18 15:40:14 -0800

    update env variables

--- src/app/firebase/firebaseConfig.js
+++ src/app/firebase/firebaseConfig.js
@@ -1,17 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: "AIzaSyBSaYCXUmsD6s5ieTKg5NbpgbIep7lOzP0",
-	authDomain: "stud-teach-8cd77.firebaseapp.com",
-	projectId: "stud-teach-8cd77",
-	storageBucket: "stud-teach-8cd77.firebasestorage.app",
-	messagingSenderId: "57490244132",
-	appId: "1:57490244132:web:75596baedbb5f04a7b8eb1",
+	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
