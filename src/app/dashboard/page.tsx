83bc90dea0441f32a99584de6b953fc102325b43
Whal0s <jordanlo8223@gmail.com>
2025-01-19 08:48:38 -0800

    Data fetch test

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,14 @@
+'use client'
+
 import RoomList from '@/components/ui/RoomList'
+import { fetchcalls } from './handleRoomSelection'
+
 
 export default function Dashboard() {
   return (
     <div className="container mx-auto px-4 py-8 pt-20">
       <h1 className="text-3xl font-bold mb-6">Get Started with a Random Room!</h1>
+        <button onClick={fetchcalls}>test</button>
       <RoomList />
     </div>
   )
