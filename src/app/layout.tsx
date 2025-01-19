edf348ddff492b28e08d7469b829c81442d3b2a9
Simoon Song <simoonsong1006@gmail.com>
2025-01-19 08:52:51 -0800

    Added font and changed some ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,29 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+// 	variable: "--font-geist-sans",
+// 	subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+// 	variable: "--font-geist-mono",
+// 	subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
-  title: "nwHacks 2025 Project",
+	title: "nwHacks 2025 Project",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={`antialiased`}>{children}</body>
+		</html>
+	);
 }
--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,60 +1,48 @@
-'use client';
+"use client";
 
-import React, { useEffect, useState } from 'react';
-import { auth } from '@/firebase/firebaseConfig';
-import { loginWithGoogle, handleLogout } from '@/utils/handleLoginOut';
-import Link from 'next/link';
-import { Button } from './button';
+import React, { useEffect, useState } from "react";
+import { auth } from "@/firebase/firebaseConfig";
+import { loginWithGoogle, handleLogout } from "@/utils/handleLoginOut";
+import Link from "next/link";
+import { Button } from "./button";
 
 const Navbar = () => {
-    const [user, setUser] = useState(auth.currentUser);
+	const [user, setUser] = useState(auth.currentUser);
 
-    useEffect(() => {
-        const unsubscribe = auth.onAuthStateChanged((user) => {
-            setUser(user);
-        });
+	useEffect(() => {
+		const unsubscribe = auth.onAuthStateChanged((user) => {
+			setUser(user);
+		});
 
-        return () => unsubscribe();
-    }, []);
+		return () => unsubscribe();
+	}, []);
 
-    return (
-        <nav className="fixed top-0 left-0 w-full border-b border-gray-200 z-50 bg-slate-100">
-            <div className="container mx-auto px-4 py-2 flex justify-between items-center ">
-                <Link href="/" className='py-2 px-3 hover:bg-slate-200 rounded'>
-                    <span className="font-bold text-lg">Student Connect</span>
-                </Link>
-                <div className="flex items-center gap-4">
-                    {user ? (
-                        <>
-                            <div className="flex items-center gap-3">
-                                {user.photoURL && (
-                                    // eslint-disable-next-line @next/next/no-img-element
-                                    <img
-                                        src={user.photoURL} 
-                                        alt="Profile" 
-                                        className="w-8 h-8 rounded-full"
-                                    />
-                                )}
-                                <Button
-                                    onClick={handleLogout}
-                                    className="text-sm hover:text-gray-800"
-                                >
-                                    Logout
-                                </Button>
-                            </div>
-                        </>
-                    ) : (
-                        <Button 
-                            onClick={loginWithGoogle}
-                            className="text-sm hover:text-gray-300"
-                        >
-                            Sign in
-                        </Button>
-                    )}
-                </div>
-            </div>
-        </nav>
-    );
+	return (
+		<nav className="fixed top-0 left-0 w-full border-b border-gray-200 z-50 bg-peach">
+			<div className="container mx-auto px-4 py-2 flex justify-between items-center ">
+				<div className="text-xl font-semibold font-custom text-moss">STUDENTCONNECT</div>
+				<div className="flex items-center gap-4">
+					{user ? (
+						<>
+							<div className="flex items-center gap-3">
+								{user.photoURL && (
+									// eslint-disable-next-line @next/next/no-img-element
+									<img src={user.photoURL} alt="Profile" className="w-8 h-8 rounded-full" />
+								)}
+								<button onClick={handleLogout} className="text-sm text-gray-900 hover:text-gray-800">
+									Logout
+								</button>
+							</div>
+						</>
+					) : (
+						<button onClick={loginWithGoogle} className="text-sm text-gray-900 hover:text-gray-800">
+							Sign in
+						</button>
+					)}
+				</div>
+			</div>
+		</nav>
+	);
 };
 
 export default Navbar;
